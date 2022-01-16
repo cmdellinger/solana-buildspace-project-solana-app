0c6a75a0c1c71fafd159586cba7fc1bb3895215d
Charles Dellinger <charles.dellinger@gmail.com>
2022-01-15 22:52:55 -0500

    updated new, generated program ID in Anchor.toml and lib.rs

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("FSyHNf7GjVgRb8eFxxP2c353JqdnvBdhVpehCbfRjCis");
 
 #[program]
 pub mod myepicproject {
